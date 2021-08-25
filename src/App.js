ca5b97e6b40e87dc125ac2cf67ab5b98cdffd9ae
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-08-25 23:31:15 +0100

    Adjusted the logic in custom hook for dependencies

--- src/App.js
+++ src/App.js
@@ -7,24 +7,25 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObject) => {
-    const loadedTasks = [];
-  
-    for (const taskKey in tasksObject) {
-      loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  }
-
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp(
-    { url: 'https://react-http-6a583-default-rtdb.europe-west1.firebasedatabase.app/tasks.json' },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+
+    const transformTasks = (tasksObject) => {
+      const loadedTasks = [];
+    
+      for (const taskKey in tasksObject) {
+        loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks({
+      url: 'https://react-http-6a583-default-rtdb.europe-west1.firebasedatabase.app/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
